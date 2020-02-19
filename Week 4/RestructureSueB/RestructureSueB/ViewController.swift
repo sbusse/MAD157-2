5346e312a1536939798b187fa536399b6aa316fd
Susan Busse <31662073+sbusse@users.noreply.github.com>
2020-02-19 12:24:22 -0600

    Add files via upload

---
+++ Week 4/RestructureSueB/RestructureSueB/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  RestructureSueB
+//
+//  Created by student on 2/15/20.
+//  Copyright © 2020 McHenry County College. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
+
