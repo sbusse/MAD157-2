859818083540e2f8c38b4cab5b82b1bc78991293
Susan Busse <31662073+sbusse@users.noreply.github.com>
2020-03-07 20:16:24 -0600

    Add files via upload

--- Week 7/simpleTableSueB/simpleTable/ViewController.swift
+++ Week 7/simpleTableSueB/simpleTable/ViewController.swift
@@ -8,40 +8,42 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDelegate {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     private let dwarves = [
     "Sleepy", "Sneezy", "Bashful", "Happy", "Doc", "Grumpy", "Dopey", "Nori", "Ori", "Balin", "Dwalin", "Fili", "oin", "Gloin", "Bidur", "Bofur", "Bombur"]
     
-    let simpleTableIndentifier = SimpleTableIdentifier
+    let simpleTableIdentifier = "SimpleTableIdentifier"
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
-    //MARK:-
-    //MARK: Table View Data Source Methods
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section:
-        Int) -> Int {
+    // MARK:- // MARK: Table View Data Source Methods
+    func tableView(_ tableView: UITableView, numberOfRowsInSection
+        section: Int) -> Int
+    {
         return dwarves.count
     }
     
-    func tableView(_tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        var cell = tableView.dequeueReusableCell(withIdentifier: simpleTableIndentifier)
-        if (cell == nil) {
-            cell = UITableViewCell(
-                style: UITableViewCellStyle.default,
-                reuseIdentifier: simpleTableIndentifier)
-        }
-        
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath:
+        IndexPath) -> UITableViewCell
+    {
+        var cell = tableView.dequeueReusableCell(withIdentifier:
+            simpleTableIdentifier);
+                if (cell == nil)
+                {
+                    cell = UITableViewCell(
+                    style: UITableViewCell.CellStyle.default,
+                    reuseIdentifier: simpleTableIdentifier)
+                }
+       
         let image = UIImage(named: "star")
         cell?.imageView?.image = image
         let highlightedImage = UIImage(named: "star2")
-        
-            cell?.imageView?.highlightedImage =
-                highlightedImage
+        cell?.imageView?.highlightedImage = highlightedImage
         
         cell?.textLabel?.text =
             dwarves[indexPath.row]
